a957f14edad62ed4e3bf1e856471c6d06962cf9f
hsae123 <79788023+hsae123@users.noreply.github.com>
2022-03-18 16:12:01 +1100

    Update report_test.py
    Removed test_success_report_with_same_article_id()

--- PHASE_1/TestScripts/report_test.py
+++ PHASE_1/TestScripts/report_test.py
@@ -11,17 +11,6 @@ def test_success_report():
         body = get_body(response)
         assert body["report_id"] == report_id
 
-def test_success_report_with_same_article_id():
-    report_id1 = 1
-    report_id2 = 2
-    response1 = requests.get(f'{url_report}report_id={report_id1}')
-    response2 = requests.get(f'{url_report}report_id={report_id2}')
-    assert response1.status_code == 200 and response2.status_code == 200
-    body1 = get_body(response1)
-    body2 = get_body(response2)
-    assert body1["report_id"] == report_id1 and body2["report_id"] == report_id2
-    assert body1["article_id"] == body2["article_id"]
-
 def test_additional_report_id():
     '''
     Tests that various types of report ID's are showing the correct exceptions
