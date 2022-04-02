2164e1423d9b7a8fee59c8235496d4ec174b1033
AustinHWalsh <austinhwalsh@gmail.com>
2022-04-03 00:19:04 +1100

    react v17 and added charts but they dont resize down currently

--- PHASE_2/frontend/src/components/GraphGrid.js
+++ PHASE_2/frontend/src/components/GraphGrid.js
@@ -1,14 +1,42 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import { StoreContext } from '../Store';
 import { Grid } from '@mui/material';
 import { GridContainer, GridElement } from './StyledComponents';
-import { LineChart } from 'reaviz';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 const data = [
-    { key: new Date('2/22/2020'), data: 14 },
-    { key: new Date('2/25/2020'), data: 5 },
-    { key: new Date('2/29/2020'), data: 18 }
-];
+    { name: "Page A", uv: 4000, pv: 2400, amt: 2400 },
+    { name: "Page B", uv: 3000, pv: 1398, amt: 2210 },
+    { name: "Page C", uv: 2000, pv: 9800, amt: 2290 },
+    { name: "Page D", uv: 2780, pv: 3908, amt: 2000 },
+    { name: "Page E", uv: 1890, pv: 4800, amt: 2181 },
+    { name: "Page F", uv: 2390, pv: 3800, amt: 2500 },
+    { name: "Page G", uv: 3490, pv: 4300, amt: 2100 }
+  ];
+
+function Graph() {
+    return (
+        <ResponsiveContainer width="99%" height={1} padding={0}>
+            <LineChart
+            data={data}
+            margin={{
+                top: 305,
+                right: 30,
+                left: 20,
+                bottom: 205,
+            }}
+            >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+            <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+            </LineChart>
+        </ResponsiveContainer>
+    );
+}
 
 function FormRow() {
     const { page, modal, country } = React.useContext(StoreContext);
@@ -16,17 +44,17 @@ function FormRow() {
         <React.Fragment>
             <Grid item xs={4}>
                 <GridElement>
-                    <LineChart height={250} width={300} data={data}/>
+                    <Graph/>
                 </GridElement>
             </Grid>
             <Grid item xs={4}>
                 <GridElement>
-                    <LineChart height={250} width={300} data={data}/>
+                    <Graph/>
                 </GridElement>
             </Grid>
             <Grid item xs={4}>
                 <GridElement>
-                    <LineChart height={250} width={300} data={data}/>
+                    <Graph/>
                 </GridElement>
             </Grid>
         </React.Fragment>
@@ -34,12 +62,9 @@ function FormRow() {
   }
 
 function GraphGrid() {
-    return (
+    return (   
         <GridContainer >
             <Grid container spacing={1}>
-                <Grid container item spacing={2}>
-                    <FormRow />
-                </Grid>
                 <Grid container item spacing={2}>
                     <FormRow />
                 </Grid>
@@ -51,4 +76,4 @@ function GraphGrid() {
     );
 }
 
-export { GraphGrid }
+export { GraphGrid, Graph }
